5c80ca63831afed51637a7c3491f61a0d0168576
Philippe <philippeguarinos@vapeuretsens.com>
2026-06-25 19:23:05 +0200

    Update Firebase config with correct app credentials

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDK1aP5xB8vJ2mK9nL3oP4qR5sT6uV7wX8",
-  authDomain: "catalogue-saveurs.firebaseapp.com",
+  apiKey: "AIzaSyB2ypnPKtw5vtfAJ8UDfGy706I0_Tnia4g",
+  authDomain: "catalogue-saveurs-1cbe3.firebaseapp.com",
   projectId: "catalogue-saveurs-1cbe3",
-  storageBucket: "catalogue-saveurs-1cbe3.appspot.com",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:abcdef1234567890ghijkl"
+  storageBucket: "catalogue-saveurs-1cbe3.firebasestorage.app",
+  messagingSenderId: "481473526096",
+  appId: "1:481473526096:web:c042a8b7ed5e78909ad455"
 };
 
 const app = initializeApp(firebaseConfig);
