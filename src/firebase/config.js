17a2e426b98f72554161adf101173cee1c021a79
Philippe <philippeguarinos@vapeuretsens.com>
2026-06-25 19:00:27 +0200

    Switch to Blaze project

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -6,8 +6,8 @@ import { getStorage } from 'firebase/storage';
 const firebaseConfig = {
   apiKey: "AIzaSyDK1aP5xB8vJ2mK9nL3oP4qR5sT6uV7wX8",
   authDomain: "catalogue-saveurs.firebaseapp.com",
-  projectId: "catalogue-saveurs",
-  storageBucket: "catalogue-saveurs.appspot.com",
+  projectId: "catalogue-saveurs-1cbe3",
+  storageBucket: "catalogue-saveurs-1cbe3.appspot.com",
   messagingSenderId: "123456789012",
   appId: "1:123456789012:web:abcdef1234567890ghijkl"
 };
