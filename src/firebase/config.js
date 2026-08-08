154d8ab3580921345fab994fad1afe37604203a6
Philippe <philippeguarinos@vapeuretsens.com>
2026-08-08 09:56:46 +0200

    Fix Firebase config to use correct project

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,9 +5,9 @@ import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyB2ypnPKtw5vtfAJ8UDfGy706I0_Tnia4g",
-  authDomain: "catalogue-saveurs-1cbe3.firebaseapp.com",
-  projectId: "catalogue-saveurs-1cbe3",
-  storageBucket: "catalogue-saveurs-1cbe3.firebasestorage.app",
+  authDomain: "catalogue-saveurs.firebaseapp.com",
+  projectId: "catalogue-saveurs",
+  storageBucket: "catalogue-saveurs.appspot.com",
   messagingSenderId: "481473526096",
   appId: "1:481473526096:web:c042a8b7ed5e78909ad455"
 };
